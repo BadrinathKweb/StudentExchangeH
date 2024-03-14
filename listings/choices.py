59bada51c44b5ee44855143513f3d62a38f96b5a
K Badrinath <badrikandikonda12@gmail.com>
2024-03-14 19:54:28 +0530

    studentexchangehub

---
+++ listings/choices.py
@@ -0,0 +1,44 @@
+category_choices = {
+  'Electronics': 'Electronics',
+  'Property': 'Property',
+  'Cars': 'Cars',
+  'Furniture': 'Furniture',
+  'Jobs': 'Jobs',
+  'Mobiles': 'Mobiles',
+  'Bikes': 'Bikes',
+  'Books&Sports': 'Books&Sports',
+  'Fashion': 'Fashion'
+  }
+
+price_choices = {
+  '1000':'₹1,000',
+  '2000':'₹2,000',
+  '5000':'₹5,000',
+  '10000':'₹10,000',
+  '20000':'₹20,000',
+  '50000':'₹50,000',
+  '100000':'₹100,000',
+  '200000':'₹200,000',
+  '500000':'₹500,000',
+  '1000000':'₹1M+',
+  
+}
+
+branch_choices = {
+  'ECE': 'ECE',
+  'CSE': 'CSE',
+  'EEE': 'EEE',
+  'MEC': 'MECH',
+  'CI': 'CIVIL',
+  'DS': 'DATA SCIENCE',
+  'IT': 'IT',
+  'AI': 'AIML',
+  'CS': 'CYBER SECURITY',
+}
+
+year_choices={
+    '1st':'1st year',
+    '2nd':'2nd year',
+    '3rd':'3rd year',
+    '4th':'4th year',
+}
