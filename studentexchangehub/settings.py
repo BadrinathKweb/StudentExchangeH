840d6d5cabb5723300aac21daa6b14a801f56776
K Badrinath <badrikandikonda12@gmail.com>
2024-03-26 15:08:17 +0530

    change9

--- studentexchangehub/settings.py
+++ studentexchangehub/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-# ALLOWED_HOSTS =[]
 
 
 # Application definition
@@ -130,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE='djano.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'studentexchangehub/static')
 ]
